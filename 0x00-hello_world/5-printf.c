46ad3263cf6e7ff72cd8e907419e1dd0270004b2
Ojay16 <juliusojemare16@gmail.com>
2023-03-16 21:59:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Return: this is 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
