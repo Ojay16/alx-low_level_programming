58ecf8124c538852db63067d43bed78ccc45441d
Ojay16 <juliusojemare16@gmail.com>
2023-03-18 20:11:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - opening function
+ *
+ * Return:this is 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
