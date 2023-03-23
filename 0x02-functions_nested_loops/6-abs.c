02e1d082cfb0c779315f4ca5b91f15d784e942d5
Ojay16 <juliusojemare16@gmail.com>
2023-03-22 20:02:19 -0700

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,17 @@
 #include "main.h"
+/**
+ * _abs - function prints absolute value
+ *
+ * @i: parameter
+ *
+ * Return: this is i
+ */
 
-:wq
-
+int _abs(int i)
+{
+	if (i < 0)
+		i = -(i);
+	else if (i >= 0)
+		i = i;
+	return (i);
+}
