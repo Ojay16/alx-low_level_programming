c9864c93125f8ea117fd584fceb48a5abbf0bebe
Ojay16 <juliusjeff16@gmail.com>
2023-06-13 17:48:36 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * _strdup - function returns a pointer to a new string which is a duplicate of the string
+ * _strdup - function returns a pointer to a new string.
  * @str: char
  * Return: 0
  */
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+	char *ooo;
+	int j, e = 0;
 
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
+	j = 0;
+	while (str[j] != '\0')
+		j++;
 
-	aaa = malloc(sizeof(char) * (i + 1));
+	ooo = malloc(sizeof(char) * (j + 1));
 
-	if (aaa == NULL)
+	if (ooo == NULL)
 		return (NULL);
 
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
+	for (e = 0; str[e]; e++)
+		ooo[e] = str[e];
 
-	return (aaa);
+	return (ooo);
 }
